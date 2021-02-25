0e03ba58c9533e00a5463fd4e64c7cd384e44814
heidysnj <heidysnunez15@gmail.com>
2021-02-25 01:11:44 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,23 +1,31 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [todo, setTodo] = useState("");
+	const [todoList, setTodoList] = useState([]);
+
+	const updateTodo = e => {
+		setTodo(e.target.value);
+	};
+
+	const addTodo = () => {
+		setTodoList([...todoList, todo]);
+	};
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
+			<p className="display-3 text-muted">
+				<em>Todos</em>
 			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
+			<input onChange={updateTodo} />
+			<button className="btn btn-light" onClick={addTodo}>
+				Add
+			</button>
 			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+				{todoList.map(item => (
+					<div>{item}</div>
+				))}
 			</p>
 		</div>
 	);
