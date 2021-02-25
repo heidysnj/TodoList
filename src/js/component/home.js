60114b9283b68245419ed7a30c7aa2e3c65a2470
heidysnj <heidysnunez15@gmail.com>
2021-02-25 02:05:29 +0000

    complete

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 export function Home() {
 	const [todo, setTodo] = useState("");
 	const [todoList, setTodoList] = useState([]);
+	let showArray = [];
 
 	const updateTodo = e => {
 		setTodo(e.target.value);
@@ -11,6 +12,7 @@ export function Home() {
 
 	const addTodo = () => {
 		setTodoList([...todoList, todo]);
+		setTodo("");
 	};
 
 	return (
@@ -18,15 +20,13 @@ export function Home() {
 			<p className="display-3 text-muted">
 				<em>Todos</em>
 			</p>
-			<input onChange={updateTodo} />
+			<input onChange={updateTodo} value={todo} />
 			<button className="btn btn-light" onClick={addTodo}>
 				Add
 			</button>
-			<p>
-				{todoList.map(item => (
-					<div>{item}</div>
-				))}
-			</p>
+			{todoList.map((item, index) => (
+				<p key={index}>{item}</p>
+			))}
 		</div>
 	);
 }
